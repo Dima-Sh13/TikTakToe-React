ee43ec4c7f94f4b47fb1244f7431a84be279fa53
Dima Shalupnya <dshalupnya@gmail.com>
2026-05-04 11:53:37 +0200

    board component init

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 export default function Player({name, symbol}) {
+  const [playerName2, setPlayerName] = useState(name);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
@@ -8,10 +9,15 @@ export default function Player({name, symbol}) {
 
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let playerName = <span className="player-name">{playerName2}</span>;
  
   if (isEditing) {
-    playerName= <input type='text' required value={name}/>;
+    playerName= <input type='text' required value={playerName2} onChange={handleChange}/>;
     
   }
 
