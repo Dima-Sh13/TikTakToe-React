8f079cb45d9c5985de89c80b734df85f72c83469
Dima Shalupnya <dshalupnya@gmail.com>
2026-05-07 11:38:35 +0200

    2 players switch developed

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Player({name, symbol}) {
+export default function Player({name, symbol, isActive }) {
   const [playerName2, setPlayerName] = useState(name);
   const [isEditing, setIsEditing] = useState(false);
 
@@ -21,7 +21,7 @@ export default function Player({name, symbol}) {
     
   }
 
-    return (<li>
+    return (<li className={isActive ? 'active' : undefined} >
           <span className="player">
             {playerName}
             <span className="player-symbol">{symbol}</span>
