4c89a15a3c2cfd97842cb96bf0d63cc5a7429a00
Dima Shalupnya <dshalupnya@gmail.com>
2026-05-22 21:59:22 +0200

    score board

--- src/components/ScoreTable.jsx
+++ src/components/ScoreTable.jsx
@@ -1,1 +1,13 @@
-able.jsx
+export default function ScoreTable({player1, player2}) {
+
+
+    return (<div>
+        <p>
+            Player 1 Score: {player1}
+        </p>
+        <p>
+            player 2 Score: {player2}
+        </p>
+
+    </div>)
+}
