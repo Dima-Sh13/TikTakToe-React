bb1aaba7ad6b0c7168ea5a048c856454901de7df
Dima Shalupnya <dshalupnya@gmail.com>
2026-06-07 13:10:37 +0200

    fix

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -3,7 +3,7 @@ export default function(cons){
     function checkerChat(params) {
 
         return console.log("{params}");
-        
+        <p>im sorry to myself, this is gonna chamge from now, i promise</p>
         
     }
 
