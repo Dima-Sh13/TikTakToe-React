08cbdef7abd71c9d78529c809eeace3e4de9b8e9
Dima Shalupnya <dshalupnya@gmail.com>
2026-06-09 13:20:53 +0200

    player fix

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -30,3 +30,33 @@ export default function Player({name, symbol, isActive }) {
         </li>)
 
 };
+
+  const [playerName2, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing );
+
+  }
+
+  function handleChange(event) {
+    console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let playerName = <span className="player-name">{playerName2}</span>;
+ 
+  if (isEditing) {
+    playerName= <input type='text' required value={playerName2} onChange={handleChange}/>;
+    
+  }
+
+    return (<li className={isActive ? 'active' : undefined} >
+          <span className="player">
+            {playerName}
+            <span className="player-symbol">{symbol}</span>
+          </span>
+          <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>        
+        </li>)
+
+};
